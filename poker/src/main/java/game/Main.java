fde47c394bb38f9cf3210267165557fd2f783730
BrinchyBoy <Anton.Brinch.AB@gmail.com>
2026-01-07 13:30:43 +0100

    Fix needed

--- poker/src/main/java/game/Main.java
+++ poker/src/main/java/game/Main.java
@@ -1,7 +1,35 @@
 package game;
 
-public class Main {
+
+import game.controller.MenuController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/src/main/resources/fxml_files/PokerMenu.fxml"));
+        Parent root = loader.load();
+    
+        // Få adgang til MenuController
+        MenuController menuController = loader.getController();
+    
+        // Kald start for at indhente input og opsætte spillet
+        menuController.start();
+
+        Scene scene = new Scene(root);
+        primaryStage.setTitle("Poker");
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Jeg tager dine penge!");
+        launch(args);
     }
 }
--- poker/src/main/java/game/controller/MenuController.java
+++ poker/src/main/java/game/controller/MenuController.java
@@ -1,5 +1,18 @@
 package game.controller;
 
-public class MenuController {
-    
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+
+public class MenuController implements Initializable{
+
+    public void start() throws IOException {
+    }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
 }
