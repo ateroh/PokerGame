2cbe160f3032ae7c8e76eceaa2b888e76ec9c41e
Lime <emil.jensen.3110@gmail.com>
2026-01-22 15:18:10 +0100

    demo

--- poker/src/main/java/game/players/PlayerClient.java
+++ poker/src/main/java/game/players/PlayerClient.java
@@ -25,7 +25,7 @@ import javafx.application.Platform;
  */
 public class PlayerClient {
 
-    protected String ip = getPublicIp();
+    protected String ip = getPublicIp();// "localhost"; //"0.0.0.0" //getPublicIp();
     protected String port;
     protected String uri;
     protected String username;
@@ -45,8 +45,8 @@ public class PlayerClient {
 
 
     public PlayerClient(String serverIp, int serverPort, String username) {
-        serverIp = "0.tcp.eu.ngrok.io";
-        serverPort = 16627;
+        serverIp = "7.tcp.eu.ngrok.io"; //"192.168.153.11"
+        serverPort = 15713;
         this.username = username;
         this.port = String.valueOf(9100 + (int)(Math.random() * 900));
         this.uri = formatURI(ip, port);
